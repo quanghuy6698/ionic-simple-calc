f493dd0fd84c54a5e6b548aa2ff639791118b17d
unknown <quanghuy6698@gmail.com>
2022-05-01 21:06:10 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'simple-calc',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
